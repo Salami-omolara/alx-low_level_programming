df6236329ace4f62256187fa8347165679c9e5b8
Salami-omolara <salamiomolara56@gmail.com>
2023-06-16 09:43:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 /**
-*main -A program that print a line with puts function 
+* main - A program that print a line with puts function 
 * Return: 0 (Success)
 */
 int main(void)
-(
-puts("\"Programming is like building a multilingual puzzle");
-return (0):
-)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
