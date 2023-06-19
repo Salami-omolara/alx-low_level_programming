c0ae9031977340a78181f7a797df9902bf67f1a1
Salami-omolara <salamiomolara56@gmail.com>
2023-06-19 15:32:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-Entry point
+*
+*Return:Always 0(Success)
+*/
+int main(void)
+{
+printf("size of a char: %dbyte(s)\n", sizeof(char));
+printf("size of an int: %dbyte(s)\n", sizeof(int));
+printf("size of a long int: %dbyte(s)\n", sizeof(long int));
+printf("size of a long long int: %dbyte(s)\n", sizeof(long long int));
+printf("size of a float: %dbyte(s)\n", sizeof(float));
+return (0);
+}
