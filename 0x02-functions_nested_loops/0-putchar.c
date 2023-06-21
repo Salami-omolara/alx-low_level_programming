32c2f8e90ba9b9f0418174f27d19778fea3aea2c
Salami-omolara <salamiomolara56@gmail.com>
2023-06-21 07:25:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
-*main-print _putchar
-*ls written by mbah
-*Return:0
+*main-Entry point
+*
+*Return:Always 0
 */
 int main(void)
-(
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -16,4 +17,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-)
+}
